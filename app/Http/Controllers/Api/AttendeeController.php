c8487cbc94cd25b92fa3f3ddedb4a69b4eefae56
giflores1026 <flores.geraldivan@gmail.com>
2024-10-03 15:16:50 +0800

    Loading Attendee Relations

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\Attendee;
@@ -12,9 +13,15 @@ use App\Models\Attendee;
 class AttendeeController extends Controller
 {
 
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+    
     public function index(Event $event)
     {
-        $attendees = $event->attendees()->latest();
+        $attendees = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
 
         return AttendeeResource::collection(
             $attendees->paginate()
@@ -23,16 +30,20 @@ class AttendeeController extends Controller
 
     public function store(Request $request, Event $event)
     {
-       $attendee = $event->attendees()->create([
-            'user_id' => 1
-       ]); 
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+                'user_id' => 1
+            ])
+        ); 
 
        return new AttendeeResource($attendee);
     }
 
     public function show(Event $event, Attendee $attendee)
     {
-        return new AttendeeResource($attendee);
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
     }
 
     public function update(Request $request, string $id)
