30c14797041e80b84e8d71844a35a3a71a534097
giflores1026 <flores.geraldivan@gmail.com>
2024-10-03 17:04:47 +0800

    Sanctum and Protecting Routes

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,6 +14,11 @@ class EventController extends Controller
 
     use CanLoadRelationships;
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
     private array $relations = ['user', 'attendees', 'attendees.user'];
     
     /**
@@ -41,7 +46,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
         return new EventResource($this->loadRelationships($event));
@@ -52,7 +57,9 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return new EventResource($this->loadRelationships($event));
+        return new EventResource(
+            $this->loadRelationships($event)
+        );
     }
 
     /**
