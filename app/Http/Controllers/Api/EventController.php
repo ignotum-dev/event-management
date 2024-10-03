4dfb2f135e267c5dfc9db43c08e11fd55a713b0d
giflores1026 <flores.geraldivan@gmail.com>
2024-10-03 10:35:20 +0800

    Attendees and Pagination

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\User;
@@ -14,7 +15,9 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(
+            Event::with('user')->paginate()
+        );
     }
 
     /**
@@ -33,7 +36,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource(($event));
     }
 
     /**
@@ -41,7 +44,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource(($event));
     }
 
     /**
@@ -57,7 +61,7 @@ class EventController extends Controller
             'end_time' => 'sometimes|date|after:start_time'
         ]));
 
-        return $event;
+        return new EventResource(($event));
 
     }
 
